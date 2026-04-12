a00271a0cf651ca1e40fa736e610f78ed3aae8a8
Dhafin Halip <dhafinhalip12@gmail.com>
2026-04-12 21:02:10 +0800

    feat: make renderListTask class at UI.js

--- js/UI.js
+++ js/UI.js
@@ -1,3 +1,14 @@
 export class UI {
-  static renderListTask() {}
+  static renderListTask(tasks) {
+    const list = document.querySelector(".task-list");
+    list.innerHTML = "";
+    tasks.forEach((t) => {
+      list.innerHTML = `
+        <li data-id="${t.id}">
+          <span>${t.title}</span>
+          <span>${t.status}</span>
+        </li>
+      `;
+    });
+  }
 }
